11f8fd8e8a6b79ba2ca3f66ec1e155d4f84f8456
Crepiks <sayazhan.onlassyn@mail.ru>
2021-10-16 14:32:27 +0600

    feat: add action button to the bottom of product details screen

--- lib/product/views/product_details_view.dart
+++ lib/product/views/product_details_view.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:home_credit_clients/common/components/back_button.dart';
 import 'package:home_credit_clients/common/constants/colors.dart';
 
@@ -21,33 +23,63 @@ class _ProductDetailsViewState extends State<ProductDetailsView> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AppColors.background,
-        body: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: const [AppBackButton()],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-                width: double.infinity,
-                height: 200,
-                decoration: const BoxDecoration(
-                    color: AppColors.white,
-                    image: DecorationImage(
-                        image:
-                            AssetImage('assets/images/coffee-machine.jpeg')))),
-            const SizedBox(
-              height: 20,
-            ),
-            const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Text("Кофемашина Philips EP3241/50",
-                    style:
-                        TextStyle(fontSize: 22, fontWeight: FontWeight.w600)))
-          ],
-        ));
+        body: Stack(alignment: AlignmentDirectional.bottomCenter, children: [
+          SizedBox(
+              height: double.infinity,
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: const [AppBackButton()],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                      width: double.infinity,
+                      height: 200,
+                      decoration: const BoxDecoration(
+                          color: AppColors.white,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/coffee-machine.jpeg')))),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Text("Кофемашина Philips EP3241/50",
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.w600))),
+                ],
+              )),
+          Container(
+              width: double.infinity,
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: CupertinoButton(
+                  onPressed: () {
+                    print("Credit button pressed");
+                  },
+                  borderRadius: const BorderRadius.all(Radius.circular(14)),
+                  color: AppColors.primary,
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 6),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          FaIcon(
+                            FontAwesomeIcons.creditCard,
+                            size: 18,
+                          ),
+                          SizedBox(width: 6),
+                          Text("Оформить в кредит")
+                        ],
+                      )),
+                ),
+              ))
+        ]));
   }
 
   scanProductBarcode() async {
