d43a47e7c468f2127d83982e98b46e1528a589a2
Azatyk <azatuk2005@mail.ru>
2021-10-16 08:58:44 +0600

    feat: add main layouts and colors

---
+++ lib/common/colors.dart
@@ -0,0 +1,8 @@
+import "package:flutter/material.dart";
+
+class AppColors {
+  static const primaryColor = Color(0xFFDD0026);
+  static const whiteColor = Color(0xFFFFFFFF);
+  static const blackColor = Color(0xFF373632);
+  static const greyColor = Color(0xFFF2F2F2);
+}
