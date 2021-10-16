74c6326837495585496dd78a246daecda1a8caf1
Crepiks <sayazhan.onlassyn@mail.ru>
2021-10-16 10:52:45 +0600

    feat: add bar code scanner dependency

--- lib/home/views/home_view.dart
+++ lib/home/views/home_view.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import 'package:home_credit_clients/common/colors.dart';
 import 'package:home_credit_clients/home/views/components/header.dart';
 import 'package:home_credit_clients/home/views/components/shop_card.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -11,48 +12,54 @@ class HomeView extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.background,
       body: ListView(
-        children: const [
-          Padding(
+        children: [
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Header(),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: GestureDetector(
+                onTap: () async {
+                  String barcodeScanRes =
+                      await FlutterBarcodeScanner.scanBarcode(
+                          'ff6666', "Отмена", true, ScanMode.BARCODE);
+                },
+                child: const ShopCard(),
+              )),
+          const SizedBox(height: 20),
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: ShopCard(),
           ),
-          SizedBox(height: 20),
-          Padding(
+          const SizedBox(height: 20),
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: ShopCard(),
           ),
-          SizedBox(height: 20),
-          Padding(
+          const SizedBox(height: 20),
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: ShopCard(),
           ),
-          SizedBox(height: 20),
-          Padding(
+          const SizedBox(height: 20),
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: ShopCard(),
           ),
-          SizedBox(height: 20),
-          Padding(
+          const SizedBox(height: 20),
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: ShopCard(),
           ),
-          SizedBox(height: 20),
-          Padding(
+          const SizedBox(height: 20),
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: ShopCard(),
           ),
-          SizedBox(height: 20),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: ShopCard(),
-          ),
-          SizedBox(height: 20),
-          Padding(
+          const SizedBox(height: 20),
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: ShopCard(),
           ),
